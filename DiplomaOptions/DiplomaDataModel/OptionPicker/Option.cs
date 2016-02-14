4671d3bce20c8252d82220bdbcd3874588076168
Mike Lewis <mike_lewis_@hotmail.com>
2016-02-13 16:44:57 -0800

    added option, yearterm, and choice classes

---
+++ DiplomaOptions/DiplomaDataModel/OptionPicker/Option.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomaDataModel.OptionPicker
+{
+    class Option
+    {
+        [Key]
+        public int OptionId { get; set; }
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ DiplomaOptions/DiplomaDataModel/OptionPicker/YearTerm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomaDataModel.OptionPicker
+{
+    class YearTerm
+    {
+        [Key]
+        public int YearTermId { get; set; }
+        public int Year { get; set; }
+        public int Term { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
